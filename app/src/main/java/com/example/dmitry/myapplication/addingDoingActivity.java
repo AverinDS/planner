fa5fd46768cc2ecf564abca2c10cbb74b6d2cfc3
AverinDS <awerin960@yandex.ru>
2017-04-18 21:15:16 +0400

    set 24-hour to TimePick

--- app/src/main/java/com/example/dmitry/myapplication/addingDoingActivity.java
+++ app/src/main/java/com/example/dmitry/myapplication/addingDoingActivity.java
@@ -2,12 +2,28 @@ package com.example.dmitry.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class addingDoingActivity extends AppCompatActivity {
+
+    TimePicker time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding_doing);
+        time = (TimePicker)findViewById(R.id.timePicker);//установка 24-часового формата
+        time.setIs24HourView(true);
+
+    }
+
+    public void onTapEditText(View v)//отвечает за автоматическое стирание текста
+    {
+        EditText text = (EditText)v;
+        if(text.getText().equals("Введите название...")){//почему-то не работает!!!
+            text.setText("");
+        }
     }
 }
